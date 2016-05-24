72a2815410449532aac18537732dacf9e673bde7
DGJ <dougaojia@ilanhai.cn>
2016-05-24 15:47:10 +0800

    new project for our memories

---
+++ OurMemories/OurMemories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OurMemories
+//
+//  Created by LanHai on 16/5/24.
+//  Copyright © 2016年 斗高甲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OurMemories/OurMemories/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OurMemories
+//
+//  Created by LanHai on 16/5/24.
+//  Copyright © 2016年 斗高甲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
